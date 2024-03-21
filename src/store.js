06045da54ab57ca11b4cf479a59a4497c7e3d6ab
MichaelZolotov77 <michaelzol@yandex.ru>
2024-03-21 18:10:24 +0300

    Подключение redux_devtools в стор

--- src/store.js
+++ src/store.js
@@ -29,7 +29,10 @@ const todos = (state = [], action) => {
 };
 
 // store
-export const store = createStore(todos);
+export const store = createStore(
+  todos,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 // action creators
 export const addTodo = (title) => ({
