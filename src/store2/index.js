32139538e525ac7fcd51a087ca2d6c4bec4cfc1a
MichaelZolotov77 <michaelzol@yandex.ru>
2024-03-23 13:29:22 +0300

    Синхронизация с localStorage

--- src/store2/index.js
+++ src/store2/index.js
@@ -1,9 +1,22 @@
 import { legacy_createStore as createStore } from "redux";
 import { devToolsEnhancer } from "redux-devtools-extension";
 import { rootReducer } from "./root-reducer";
+import { loadState, saveState } from "./local-storage";
+import { throttle } from "lodash";
 
 // export const store = createStore(rootReducer, devToolsEnhancer());
 
 export const configureStore = () => {
-  return createStore(rootReducer, devToolsEnhancer());
+  const persistedState = loadState();
+  const store = createStore(rootReducer, persistedState, devToolsEnhancer());
+
+  store.subscribe(
+    throttle(() => {
+      saveState({
+        todos: store.getState().todos,
+      });
+    }, 1000)
+  );
+
+  return store;
 };
